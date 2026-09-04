fd2a2bb15b7efa49072454636c7a80d1030426d8
NaritaRadu <naritaradu0@gmail.com>
2026-09-04 18:23:27 +0300

    added views and some templates

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+
+from .models import Question
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello world")
+    latest_question_list=Question.objects.order_by("-publication_date")[:5]
+    return render(request,"polls/home.html",{"latest_question_list":latest_question_list})
+
+def detail(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request,"polls/detail.html",{"question":question})
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+def vote(request,question_id):
+    return HttpResponse(f"You are voting on question {question_id}")
