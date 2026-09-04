ba3435889bae3e514786b74f5c2b4a6d45acf934
NaritaRadu <naritaradu0@gmail.com>
2026-09-04 19:44:48 +0300

    added a static folder and some style

--- polls/views.py
+++ polls/views.py
@@ -11,7 +11,7 @@ class HomeView(ListView):
     template_name="polls/home.html"
     context_object_name="latest_question_list"
     def get_queryset(self):
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+        return Question.objects.filter(publication_date__lte=timezone.now()).order_by("-publication_date")[:5]
     
 
 class DetailView(DetailView):
