5b2224263a02a9cd5878d2b7fedec006fc8744b2
NaritaRadu <naritaradu0@gmail.com>
2026-09-04 20:16:08 +0300

    Changing the admin site

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Question,Choice
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model=Choice
+    extra=3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets=[
+        (None,{"fields":["question_text"]}),("Date Information",{"fields":["publication_date"],"classes":["collapse"]}),
+    ]
+    inlines=[ChoiceInline]
+    list_display = ["question_text", "publication_date"]
+
+admin.site.register(Question,QuestionAdmin)
